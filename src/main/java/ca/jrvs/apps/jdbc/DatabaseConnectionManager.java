75340199ebafe181a7a12083df79a96c03c35328
steve-ma-jrvs <steve.ma.jrvs.ca@gmail.com>
2019-07-02 10:56:18 -0400

    2-jdbc

---
+++ src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
@@ -0,0 +1,14 @@
+package ca.jrvs.apps.jdbc;
+
+import javax.xml.crypto.Data;
+import java.util.Properties;
+
+public class DatabaseConnectionManager {
+    private final String url;
+    private final Properties properties;
+
+    public DatabaseConnectionManager(String host, String databaseName,
+                                     String username, String password){
+
+    }
+}
